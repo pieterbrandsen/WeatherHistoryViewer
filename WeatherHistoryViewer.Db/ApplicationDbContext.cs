5c6bcf4886cc0f6636fa2253a0ee24d68d02228f
Pieter <pieterbrandsen2@gmail.com>
2021-01-23 16:08:21 +0100

    Added Database
    Currently still place for change.

---
+++ WeatherHistoryViewer.Db/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WeatherHistoryViewer.Core;
+
+namespace WeatherHistoryViewer.Db
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=weatherDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        public DbSet<CurrentWeather> Weather { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
