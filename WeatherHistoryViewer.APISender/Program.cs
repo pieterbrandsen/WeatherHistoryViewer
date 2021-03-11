dd45dcb3ec774fc96ad7d650fcae28cc62aabd3b
PieterBrandsen <pieterbrandsen2@gmail.com>
2021-03-11 11:38:22 +0100

    Added keyvault

--- WeatherHistoryViewer.APISender/Program.cs
+++ WeatherHistoryViewer.APISender/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace WeatherHistoryViewer.APISender
 {
@@ -13,6 +16,13 @@ namespace WeatherHistoryViewer.APISender
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(
+keyVaultEndpoint,
+new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
         }
     }
